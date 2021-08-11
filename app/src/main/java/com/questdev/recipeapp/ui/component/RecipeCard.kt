797f208ef980447b84d5ec15811e335981c77c3a
James Eissah Aidoo <jamesaidooessah@gmail.com>
2021-08-11 16:33:05 +0000

    Feature update: Issue #6
    Load recipe images from their urls. Change the `Image` composable to `GlideImage`.

--- app/src/main/java/com/questdev/recipeapp/ui/component/RecipeCard.kt
+++ app/src/main/java/com/questdev/recipeapp/ui/component/RecipeCard.kt
@@ -1,6 +1,5 @@
 package com.questdev.recipeapp.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
@@ -10,13 +9,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.questdev.domain.model.Recipe
 import com.questdev.recipeapp.R
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
@@ -29,13 +30,16 @@ fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
         elevation = 4.dp
     ) {
         Column {
-            Image(
-                painter = painterResource(R.drawable.empty_plate),
+            GlideImage(
+                imageModel = recipe.featuredImage,
                 contentDescription = stringResource(R.string.recipe_image),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(225.dp),
-                contentScale = ContentScale.Crop
+                circularRevealedEnabled = true,
+                contentScale = ContentScale.Crop,
+                placeHolder = ImageBitmap.imageResource(id = R.drawable.empty_plate),
+                error = ImageBitmap.imageResource(id = R.drawable.empty_plate),
             )
             Divider(color = MaterialTheme.colors.primary, thickness = 1.dp)
             Row(
